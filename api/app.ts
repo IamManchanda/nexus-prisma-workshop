7b256c712fb782cdd7790c2caf0fd491b2ac13f6
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-13 16:36:19 +0530

    Move schema and server code to app.ts

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,17 @@
-import { server } from "./server";
+import { makeSchema } from "@nexus/schema";
+import { join } from "path";
+import { ApolloServer } from "apollo-server";
+import * as typeDefs from "./graphql";
+
+const schema = makeSchema({
+  types: typeDefs,
+  outputs: {
+    typegen: join(__dirname, "..", "nexus-typegen.ts"),
+    schema: join(__dirname, "..", "schema.graphql"),
+  },
+});
+
+const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
--- nexus-typegen.ts
+++ nexus-typegen.ts
@@ -3,58 +3,65 @@
  * Do not make changes to this file directly
  */
 
+
+
+
+
+
+
 declare global {
   interface NexusGen extends NexusGenTypes {}
 }
 
-export interface NexusGenInputs {}
+export interface NexusGenInputs {
+}
 
-export interface NexusGenEnums {}
+export interface NexusGenEnums {
+}
 
 export interface NexusGenScalars {
-  String: string;
-  Int: number;
-  Float: number;
-  Boolean: boolean;
-  ID: string;
+  String: string
+  Int: number
+  Float: number
+  Boolean: boolean
+  ID: string
 }
 
 export interface NexusGenRootTypes {
-  Post: {
-    // root type
+  Post: { // root type
     body?: string | null; // String
     id?: number | null; // Int
     published?: boolean | null; // Boolean
     title?: string | null; // String
-  };
+  }
   Query: {};
 }
 
 export interface NexusGenAllTypes extends NexusGenRootTypes {
-  String: NexusGenScalars["String"];
-  Int: NexusGenScalars["Int"];
-  Float: NexusGenScalars["Float"];
-  Boolean: NexusGenScalars["Boolean"];
-  ID: NexusGenScalars["ID"];
+  String: NexusGenScalars['String'];
+  Int: NexusGenScalars['Int'];
+  Float: NexusGenScalars['Float'];
+  Boolean: NexusGenScalars['Boolean'];
+  ID: NexusGenScalars['ID'];
 }
 
 export interface NexusGenFieldTypes {
-  Post: {
-    // field return type
+  Post: { // field return type
     body: string | null; // String
     id: number | null; // Int
     published: boolean | null; // Boolean
     title: string | null; // String
-  };
-  Query: {
-    // field return type
-    drafts: Array<NexusGenRootTypes["Post"] | null>; // [Post]!
-  };
+  }
+  Query: { // field return type
+    drafts: Array<NexusGenRootTypes['Post'] | null>; // [Post]!
+  }
 }
 
-export interface NexusGenArgTypes {}
+export interface NexusGenArgTypes {
+}
 
-export interface NexusGenAbstractResolveReturnTypes {}
+export interface NexusGenAbstractResolveReturnTypes {
+}
 
 export interface NexusGenInheritedFields {}
 
@@ -84,28 +91,19 @@ export interface NexusGenTypes {
   interfaceNames: NexusGenInterfaceNames;
   scalarNames: NexusGenScalarNames;
   unionNames: NexusGenUnionNames;
-  allInputTypes:
-    | NexusGenTypes["inputNames"]
-    | NexusGenTypes["enumNames"]
-    | NexusGenTypes["scalarNames"];
-  allOutputTypes:
-    | NexusGenTypes["objectNames"]
-    | NexusGenTypes["enumNames"]
-    | NexusGenTypes["unionNames"]
-    | NexusGenTypes["interfaceNames"]
-    | NexusGenTypes["scalarNames"];
-  allNamedTypes:
-    | NexusGenTypes["allInputTypes"]
-    | NexusGenTypes["allOutputTypes"];
-  abstractTypes: NexusGenTypes["interfaceNames"] | NexusGenTypes["unionNames"];
+  allInputTypes: NexusGenTypes['inputNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['scalarNames'];
+  allOutputTypes: NexusGenTypes['objectNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['unionNames'] | NexusGenTypes['interfaceNames'] | NexusGenTypes['scalarNames'];
+  allNamedTypes: NexusGenTypes['allInputTypes'] | NexusGenTypes['allOutputTypes']
+  abstractTypes: NexusGenTypes['interfaceNames'] | NexusGenTypes['unionNames'];
   abstractResolveReturn: NexusGenAbstractResolveReturnTypes;
 }
 
+
 declare global {
-  interface NexusGenPluginTypeConfig<TypeName extends string> {}
-  interface NexusGenPluginFieldConfig<
-    TypeName extends string,
-    FieldName extends string
-  > {}
-  interface NexusGenPluginSchemaConfig {}
+  interface NexusGenPluginTypeConfig<TypeName extends string> {
+  }
+  interface NexusGenPluginFieldConfig<TypeName extends string, FieldName extends string> {
+  }
+  interface NexusGenPluginSchemaConfig {
+  }
 }
